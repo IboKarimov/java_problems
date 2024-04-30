160b7765e00293c02b07aaab2077effc986c44f2
IboKarimov <151522934+IboKarimov@users.noreply.github.com>
2024-05-01 00:48:49 +0400

    Update pifagor.java

--- pifagor.java
+++ pifagor.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
 
+public class Main{
+    public static void main(String[] args){
+        double a, b, c;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter side a: ");
+        a = scanner.nextDouble();
+        System.out.print("Enter side b: ");
+        b = scanner.nextDouble();
+        c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        System.out.println("The hypothenus is " + c);
+        scanner.close();
+    }
+}
